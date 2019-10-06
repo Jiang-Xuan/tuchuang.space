3a5e296db4a1e94c2aed2e3e891409b0c9dbbcd0
jiangxuan <jiangxuan@dxy.cn>
2019-10-06 14:43:30 +0800

    headless chrome pptr test

--- frondend/jest-puppeteer.config.js
+++ frondend/jest-puppeteer.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   launch: {
     dumpio: true,
-    headless: false,
+    headless: process.env.CI === 'true',
     devtools: process.env.CI !== 'true'
   },
   browserContext: 'default'
