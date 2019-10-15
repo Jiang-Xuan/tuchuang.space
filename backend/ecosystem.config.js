ff60cccbbca28ac7bcae1674831b4fd19cad9cd2
jiangxuan <jiangxuan@dxy.cn>
2019-10-15 16:56:55 +0800

    test

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '500M',
+    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
