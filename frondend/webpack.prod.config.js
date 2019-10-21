7844f02bd9c17556983c48aac1b7ef7426fd7cd6
jiangxuan <jiangxuan@dxy.cn>
2019-10-21 16:07:10 +0800

    前端 cdn path

--- frondend/webpack.prod.config.js
+++ frondend/webpack.prod.config.js
@@ -1,8 +1,17 @@
 const webpackMerge = require('webpack-merge')
 const webpackBaseConfig = require('./webpack.base.config')
 
+const { DEPLOY_TYPE } = process.env
+
+const publicPath = DEPLOY_TYPE === 'beta'
+  ? 'https://beta-assets-tuchuang-space.oss-cn-hangzhou.aliyuncs.com'
+  : 'assets-tuchuang-space.oss-cn-hangzhou.aliyuncs.com'
+
 const webpackDevConfig = {
   devtool: 'source-map',
+  output: {
+    publicPath
+  },
   mode: 'production',
   module: {
 
