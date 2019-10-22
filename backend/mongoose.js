731e7045b930fc096e8492d248a79513cf254305
jiangxuan <jiangxuan@dxy.cn>
2019-10-22 17:06:13 +0800

    修复 MongoDB 路径

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose')
 
+const { NODE_ENV } = process.env
+
+let dbPath
+
+if (NODE_ENV === 'beta') {
+  // beta 环境
+  dbPath = 'mongodb://localhost:27017/beta.tuchuang.space'
+} else if (NODE_ENV === 'production') {
+  // 生产环境
+  dbPath = 'mongodb://localhost:27017/tuchuang.space'
+} else {
+  // 本地开发环境
+  dbPath = 'mongodb://localhost:3402/test'
+}
+
 module.exports.connectDb = function () {
   return new Promise((resolve, reject) => {
     const readyState = mongoose.connection.readyState
@@ -8,7 +23,7 @@ module.exports.connectDb = function () {
       resolve()
       return
     }
-    mongoose.connect('mongodb://localhost:3402/test', { useNewUrlParser: true })
+    mongoose.connect(dbPath, { useNewUrlParser: true })
 
     mongoose.connection.once('open', resolve)
 
