8918da9bf3c8f6c9a16a58d08837b5b67753c77c
jiangxuan <jiangxuan@dxy.cn>
2019-10-26 19:48:16 +0800

    fix https://github.com/Jiang-Xuan/tuchuang.space/issues/1#issuecomment-546592051

--- frondend/e2e/pptr.test.js
+++ frondend/e2e/pptr.test.js
@@ -4,8 +4,15 @@
 // 在访问 index.html
 // 然后进行测试
 const path = require('path')
-
-const indexHtml = path.resolve(__dirname, '../dist/index.html')
+const express = require('express')
+const stoppable = require('stoppable')
+const http = require('http')
+const testApp = express()
+/** @type {10001} 用来托管 dist 目录的端口 */
+const distPort = 10001
+const distPath = path.resolve(__dirname, '../dist')
+testApp.use(express.static(distPath))
+const testServer = stoppable(http.createServer(testApp), 0)
 
 const E2E_TEST_ID_ATTR_NAME = 'data-e2e-test-id'
 
@@ -43,6 +50,9 @@ describe('上传图片时. 向 api/1.0.0/image 接口发起 POST 请求, 且请�
   })
 
   beforeAll(async () => {
+    await new Promise(resolve => {
+      testServer.listen(distPort, () => resolve())
+    })
     await page.setRequestInterception(true)
     page.on('request', (interceptedRequest) => {
       if (
@@ -54,8 +64,13 @@ describe('上传图片时. 向 api/1.0.0/image 接口发起 POST 请求, 且请�
       interceptedRequest.continue()
     })
   })
+  afterAll(async () => {
+    await new Promise((resolve, reject) => {
+      testServer.stop(error => error ? reject(error) : resolve())
+    })
+  })
   it('点击 UPLOAD_CLICK_AREA 发起相应的请求', async () => {
-    await page.goto(`file://${indexHtml}`, {
+    await page.goto(`http://127.0.0.1:${distPort}`, {
       waitUntil: 'domcontentloaded'
     })
     const fileElement = await page.$(`[${E2E_TEST_ID_ATTR_NAME}="${TEST_ID_STORE.UPLOAD_CLICK_AREA}"]`)
@@ -72,11 +87,21 @@ describe('上传图片时. 向 api/1.0.0/image 接口发起 POST 请求, 且请�
 })
 
 describe('上传文件不合法的时候展示错误信息', () => {
+  beforeAll(async () => {
+    await new Promise(resolve => {
+      testServer.listen(distPort, () => resolve())
+    })
+  })
+  afterAll(async () => {
+    await new Promise((resolve, reject) => {
+      testServer.stop(error => error ? reject(error) : resolve())
+    })
+  })
   beforeEach(async () => {
     await global.jestPuppeteer.resetBrowser()
   })
   it('图片尺寸过大', async () => {
-    await page.goto(`file://${indexHtml}`, {
+    await page.goto(`http://127.0.0.1:${distPort}`, {
       waitUntil: 'domcontentloaded'
     })
     const fileElement = await page.$(`[${E2E_TEST_ID_ATTR_NAME}="${TEST_ID_STORE.UPLOAD_CLICK_AREA}"]`)
@@ -93,7 +118,7 @@ describe('上传文件不合法的时候展示错误信息', () => {
     expect(uploadResultsInnerTextJsonValue.indexOf('不能上传 16.1m.jpeg, 原因如下:\n文件超过最大限制, 最大限制为 10M')).not.toEqual(-1)
   })
   it('格式不支持', async () => {
-    await page.goto(`file://${indexHtml}`, {
+    await page.goto(`http://127.0.0.1:${distPort}`, {
       waitUntil: 'domcontentloaded'
     })
     const fileElement = await page.$(`[${E2E_TEST_ID_ATTR_NAME}="${TEST_ID_STORE.UPLOAD_CLICK_AREA}"]`)
@@ -113,8 +138,11 @@ describe('上传文件不合法的时候展示错误信息', () => {
 
 describe('接口数据响应正常的时候显示正常的数据', () => {
   beforeAll(async () => {
+    await new Promise(resolve => {
+      testServer.listen(distPort, () => resolve())
+    })
     await global.jestPuppeteer.resetBrowser()
-    await page.goto(`file://${indexHtml}`, {
+    await page.goto(`http://127.0.0.1:${distPort}`, {
       waitUntil: 'domcontentloaded'
     })
     await page.setRequestInterception(true)
@@ -150,6 +178,12 @@ describe('接口数据响应正常的时候显示正常的数据', () => {
     await fileChooser.accept([path.resolve(__dirname, '../../shared/test_images/png.png')])
 
     await new Promise((resolve) => setTimeout(resolve, 1000))
+  })
+
+  afterAll(async () => {
+    await new Promise((resolve, reject) => {
+      testServer.stop(error => error ? reject(error) : resolve())
+    })
   })
 
   it('html 数据', async () => {
@@ -195,16 +229,25 @@ describe('接口数据响应正常的时候显示正常的数据', () => {
 
 describe('导航条导航', () => {
   beforeAll(async () => {
+    await new Promise(resolve => {
+      testServer.listen(distPort, () => resolve())
+    })
     await global.jestPuppeteer.resetBrowser()
-    await page.goto(`file://${indexHtml}`, {
+    await page.goto(`http://127.0.0.1:${distPort}`, {
       waitUntil: 'domcontentloaded'
+    })
+  })
+  afterAll(async () => {
+    await new Promise((resolve, reject) => {
+      testServer.stop(error => error ? reject(error) : resolve())
     })
   })
   it('前往 api 文档的链接正常', async () => {
     const gotoApiDocBtn = await page.$(`[${E2E_TEST_ID_ATTR_NAME}="${TEST_ID_STORE.GOTO_API_DOC_BTN}"]`)
     await gotoApiDocBtn.click()
-    await page.waitForNavigation()
     const pathname = await page.waitForFunction('location.pathname')
-    expect(pathname).toEqual('/api')
+    const pathnameJsonValue = await pathname.jsonValue()
+
+    expect(pathnameJsonValue).toEqual('/api')
   })
 })
