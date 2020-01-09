c1c4577319d75274f2acec4c26f3f884626717c2
jiangxuan <jiangxuan@dxy.cn>
2020-01-09 19:11:19 +0800

    test

--- frondend/bff/service.js
+++ frondend/bff/service.js
@@ -1,6 +1,9 @@
 const app = require('./app')
+const { BFF_LISTEN_PORT } = require('../../self-hosted.config').BFF
 
-const PORT = process.env.PORT || 4303
+const DEFAULT_BFF_LISTEN_PORT = 4303
+
+const PORT = BFF_LISTEN_PORT || DEFAULT_BFF_LISTEN_PORT
 
 app.listen(PORT, () => {
   console.log(`server is listening at ${PORT} port`)
