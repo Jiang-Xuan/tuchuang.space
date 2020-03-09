6798e519582fdefdf41b378d67302eddf84fe591
jiangxuan <jiangxuan@dxy.cn>
2020-03-09 11:53:45 +0800

    fix: 修复 typo

--- .github/actions/frondend-windows-test/main.js
+++ .github/actions/frondend-windows-test/main.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-  core.setOuput('test', (new Date()).toLocaleTimeString())
+  core.setOutput('test', (new Date()).toLocaleTimeString())
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`the event payload: ${payload}`)
 } catch (error) {
