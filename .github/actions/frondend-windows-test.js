3bbf5c87eabc72ebdacfe1c648cc216bdf29b469
jiangxuan <jiangxuan@dxy.cn>
2020-03-09 11:21:27 +0800

    test

---
+++ .github/actions/frondend-windows-test.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+  core.setOuput('test', (new Date()).toLocaleTimeString())
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`the event payload: ${payload}`)
+} catch (error) {
+  core.setFailed(error.message)
+}
