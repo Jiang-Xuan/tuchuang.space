761b5c7c3e59a8c17b0ce5878b80cfbf537015ee
jiangxuan <jiangxuan@dxy.cn>
2020-03-09 21:35:33 +0800

    save

---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+if (
+  !fs.existsSync(
+    path.resolve(__dirname, './self-hosted.config.js')
+  )
+) {
+  throw new Error('self-hosted.config.js 文件不存在, 请查阅文档 https://www.example.com')
+}
+
+const selfHostedConfig = require('./self-hosted.config')
+
+const resultConfig = {}
+
+const bff = selfHostedConfig.bff
+
+if (!!bff === false) {
+  /** bff 层的默认配置 */
+  resultConfig.bff = {
+    listenPort: 4303
+  }
+} else if (Object.prototype.toString.call(bff) !== '[object Object]') {
+  throw new Error('bff 配置无效, 请查阅文档 https://www.example.com')
+} else {
+
+}
