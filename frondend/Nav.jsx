cb7f1d290996cac44dc5213c133ab43fc29a10ca
jiangxuan <jiangxuan@dxy.cn>
2020-04-01 17:43:16 +0800

    fix: 修改文案

--- frondend/Nav.jsx
+++ frondend/Nav.jsx
@@ -12,7 +12,7 @@ function Nav (props) {
       <div className='logo' />
       <Menu mode='horizontal' theme='dark' defaultSelectedKeys={['home']} selectedKeys={[pathname]} style={{ lineHeight: '64px' }}>
         <Menu.Item key='/'><Link to='/'>上传</Link></Menu.Item>
-        <Menu.Item key='/about'><Link to='/about'>关于我</Link></Menu.Item>
+        <Menu.Item key='/about'><Link to='/about'>关于</Link></Menu.Item>
         <Menu.Item key='/contact' data-e2e-test-id='GOTO_CONTACT_BTN'><Link to='/contact'>联系我</Link></Menu.Item>
         <Menu.Item data-e2e-test-id='GOTO_API_DOC_BTN' key='/api-doc'><Link to='/api-doc'>开发者</Link></Menu.Item>
       </Menu>
